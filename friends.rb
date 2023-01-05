14688018d6061d4550d15cbce2f4193935d28232
Reid Miller <reid.s.miller@gmail.com>
2023-01-04 19:44:19 -0700

    Add second friend option

--- friends.rb
+++ friends.rb
@@ -1,7 +1,14 @@
-best_friend = "Dyson"
+best_friend = "Frank"
+good_friend = "Fran"
 
-if best_friend == "Dyson"
-    puts "Hello bestest friend ever!"
+if best_friend == "Dyson" && good_friend == "Crow"
+    puts "Hello bestest friend ever and goodest friend ever!"
+elsif good_friend == "Crow"
+    puts "Hello goodest friend ever!"
+elsif best_friend == "Dyson"
+    puts "Hellow bestest friend ever!"
 else
     puts "Begone imposter!!!!!!"
 end
+
+
